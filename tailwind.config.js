6e286025e6e90e9798c65e7d3314380f75507632
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 10:32:10 -0600

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{html,ts}',
+    './src/components/**/*.{html,ts}',
+    './src/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
