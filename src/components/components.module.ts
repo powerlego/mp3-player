de02e2795e82f589fc359d066d834a6da1fb944a
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 13:10:22 -0600

    last commit of angular, switching to react

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainWindowComponent } from './main-window/main-window.component';
 import { MediaControlsComponent } from './media-controls/media-controls.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 
 
@@ -10,13 +11,16 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   declarations: [
     MainWindowComponent,
     MediaControlsComponent,
-    SidebarComponent
+    SidebarComponent,
+    TopBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    MainWindowComponent
+    MainWindowComponent,
+    MediaControlsComponent,
+    SidebarComponent,
   ]
 })
 export class ComponentsModule { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/app/**/*.{html,ts}',
-    './src/components/**/*.{html,ts}',
-    './src/index.html',
+    "./src/app/**/*.{html,ts}",
+    "./src/components/**/*.{html,ts}",
+    "./src/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: "#16171A",
+          800: "#1D1E21",
+          700: "#25272B",
+          600: "#393D42",
+          500: "#6B7280",
+          400: "#B6B8BD",
+          300: "#CFD1D4",
+          200: "#DADCDE",
+          100: "#E3E4E6",
+        },
+        green: {
+          900: "#161A0D",
+          800: "#1C2111",
+          700: "#252B16",
+          600: "#384221",
+          500: "#6C8040",
+          400: "#A0BD5E",
+          300: "#B4D46A",
+          200: "#BCDE6F",
+          100: "#C3E673",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
