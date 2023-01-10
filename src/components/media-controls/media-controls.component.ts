2e65dbbf2755264bd792fe1f44ceb9af02180c5f
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 09:41:25 -0600

    added media controls component

---
+++ src/components/media-controls/media-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-controls',
+  templateUrl: './media-controls.component.html',
+  styleUrls: ['./media-controls.component.css']
+})
+export class MediaControlsComponent {
+
+}
