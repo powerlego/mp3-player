49935dfe797fbe07171760e6e55aa4fa6fffa66a
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-11 11:32:07 -0600

    added play button icon

--- src/components/MediaControls/index.tsx
+++ src/components/MediaControls/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { ReactComponent as PlayIcon } from "../../assets/icons/play.svg";
 
 function MediaControls() {
   return (
     <footer className="media-controls">
-
-      <footer className="media-controls__container"> </footer>
+      <div className="media-controls__container">
+        <PlayIcon className="media-icon"/>
+      </div>
     </footer>
   );
 }
-
 
 export default MediaControls;
