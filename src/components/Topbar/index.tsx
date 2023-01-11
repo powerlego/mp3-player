778dae82a215a720195e3692d087d95a4e1905e9
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 21:45:47 -0600

    added ability to toggle dark mode manually

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import useDarkMode from "../../hooks/useDarkMode";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 function Topbar() {
   return (
     <div className="top-navigation">
-      <span></span>
+      <ThemeIcon />
     </div>
   );
 }
+
+const ThemeIcon = () => {
+  const [darkTheme, setDarkTheme] = useDarkMode();
+  const handleMode = () => setDarkTheme(!darkTheme);
+  return (
+    <span onClick={handleMode}>
+      {darkTheme ? (
+        <FaSun size="24" className="top-navigation-icon" />
+      ) : (
+        <FaMoon size="24" className="top-navigation-icon" />
+      )}
+    </span>
+  );
+};
 
 export default Topbar;
---
+++ src/hooks/useDarkMode.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key:any, initialValue?:any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value:any ) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+};
+
+const useDarkMode = () => {
+  const [enabled, setEnabled] = useLocalStorage("dark-theme");
+  const isEnabled = typeof enabled !== "undefined" ? enabled : true;
+
+  useEffect(() => {
+    const className = "dark";
+    const bodyClass = window.document.body.classList;
+
+    isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+  }, [enabled, isEnabled]);
+
+  return [enabled, setEnabled];
+};
+
+export default useDarkMode;
