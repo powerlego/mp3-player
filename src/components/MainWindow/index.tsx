565bafefd1c172cff62a5bc32dd6cf5d758b49ba
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 18:04:55 -0600

    added filler content to help with styling

--- src/components/MainWindow/index.tsx
+++ src/components/MainWindow/index.tsx
@@ -3,67 +3,76 @@ import Topbar from "../Topbar";
 
 function MainWindow() {
   return (
-    <div className="content-container">
+    <div>
       <Topbar />
-      <div className='content-list'>
-        <Post
-          name='Ada'
-          timestamp='one week ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+      <div className="content">
+        <div className="content-container">
+          <div className="h-16 sticky top-0" />
+          <div className="content-list">
+            <Post
+              name="Ada"
+              timestamp="one week ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
           amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
           ipsum dolor sit amet consectetur adipisicing elit.`}
-        />
-        <Post name='Leon' timestamp='one week ago' text={`Lorem ipsum dolor. `} />
-        <Post name='Jill' timestamp='5 days ago' text={`Lorem.`} />
-        <Post
-          name='Ellie'
-          timestamp='4 days ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
-        />
-        <Post
-          name='Chris'
-          timestamp='4 days ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+            />
+            <Post
+              name="Leon"
+              timestamp="one week ago"
+              text={`Lorem ipsum dolor. `}
+            />
+            <Post name="Jill" timestamp="5 days ago" text={`Lorem.`} />
+            <Post
+              name="Ellie"
+              timestamp="4 days ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
+            />
+            <Post
+              name="Chris"
+              timestamp="4 days ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
           amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
           ipsum dolor sit amet consectetur adipisicing elit.
           
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
           ipsum dolor sit amet consectetur adipisicing elit.`}
-        />
-        <Post
-          name='Claire'
-          timestamp='2 days ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+            />
+            <Post
+              name="Claire"
+              timestamp="2 days ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
           amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
-        />
-        <Post
-          name='Albert'
-          timestamp='22 hours ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
-        />
-        <Post
-          name='Rebecca'
-          timestamp='3 hours ago'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+            />
+            <Post
+              name="Albert"
+              timestamp="22 hours ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
+            />
+            <Post
+              name="Rebecca"
+              timestamp="3 hours ago"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
           amet consectetur adipisicing elit.`}
-        />
-        <Post
-          name='H.U.N.K'
-          timestamp='Just now'
-          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+            />
+            <Post
+              name="H.U.N.K"
+              timestamp="Just now"
+              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
           amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
           ipsum dolor sit amet consectetur adipisicing elit.`}
-        />
-      </div>    
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-const Post = ({ name, timestamp, text }:any) => {
+const Post = ({ name, timestamp, text }: any) => {
   const seed = Math.round(Math.random() * 100);
   return (
     <div className={"post"}>
