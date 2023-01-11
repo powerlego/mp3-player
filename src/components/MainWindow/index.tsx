42044a28f78e15f14babae442681b6e72ca09520
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-10 21:45:35 -0600

    removed unnecessary elements

--- src/components/MainWindow/index.tsx
+++ src/components/MainWindow/index.tsx
@@ -3,96 +3,16 @@ import Topbar from "../Topbar";
 
 function MainWindow() {
   return (
-    <div>
+    <div className="h-full">
       <Topbar />
       <div className="content">
-        <div className="content-container">
-          <div className="h-16 sticky top-0" />
-          <div className="content-list">
-            <Post
-              name="Ada"
-              timestamp="one week ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.`}
-            />
-            <Post
-              name="Leon"
-              timestamp="one week ago"
-              text={`Lorem ipsum dolor. `}
-            />
-            <Post name="Jill" timestamp="5 days ago" text={`Lorem.`} />
-            <Post
-              name="Ellie"
-              timestamp="4 days ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
-            />
-            <Post
-              name="Chris"
-              timestamp="4 days ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.`}
-            />
-            <Post
-              name="Claire"
-              timestamp="2 days ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
-            />
-            <Post
-              name="Albert"
-              timestamp="22 hours ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
-            />
-            <Post
-              name="Rebecca"
-              timestamp="3 hours ago"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit.`}
-            />
-            <Post
-              name="H.U.N.K"
-              timestamp="Just now"
-              text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.`}
-            />
-          </div>
+        <div className="h-16 sticky top-0" />
+        <div className="content-list">
+          <p className="content-item">Hello</p>
         </div>
       </div>
     </div>
   );
 }
-
-const Post = ({ name, timestamp, text }: any) => {
-  const seed = Math.round(Math.random() * 100);
-  return (
-    <div className={"post"}>
-      <div className="avatar-wrapper">
-        <img
-          src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`}
-          alt=""
-          className="avatar"
-        />
-      </div>
-
-      <div className="post-content">
-        <p className="post-owner">
-          {name}
-          <small className="timestamp">{timestamp}</small>
-        </p>
-        <p className="post-text">{text}</p>
-      </div>
-    </div>
-  );
-};
 
 export default MainWindow;
