f26a195422c1eef464933523ae359ada9f6fb22d
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-11 21:11:36 -0600

    added other media controls

--- src/components/MediaControls/index.tsx
+++ src/components/MediaControls/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { ReactComponent as PlayIcon } from "../../assets/icons/play.svg";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { BsFillSkipEndFill } from "react-icons/bs";
+import { BsFillSkipStartFill } from "react-icons/bs";
+import { BiShuffle } from "react-icons/bi";
+import { TbRepeat } from "react-icons/tb";
+
 
 function MediaControls() {
   return (
     <footer className="media-controls">
       <div className="media-controls__container">
-        <PlayIcon className="media-icon"/>
+        <BiShuffle className="media-icon shuffle-button" />
+        <BsFillSkipStartFill className="media-icon skip-button" />
+        <BsFillPlayCircleFill className="media-icon play-button" />
+        <BsFillSkipEndFill className="media-icon skip-button" />
+        <TbRepeat className="media-icon repeat-button" />
       </div>
     </footer>
   );
