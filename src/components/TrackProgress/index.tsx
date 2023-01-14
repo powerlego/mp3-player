e923c05e275221b2d0c3a7fb7e804abea95b5108
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-14 15:31:53 -0600

    added duration component to display total duration of the track

--- src/components/TrackProgress/index.tsx
+++ src/components/TrackProgress/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Duration from "../Duration";
 import ProgressBar from "../ProgressBar";
 import "./TrackProgress.css";
 
@@ -14,7 +15,12 @@ class TrackProgress extends Component<TrackProgressProps> {
       <div className="media-controls__progress">
         <div className="media-controls__progress__time"> 99:99 </div>
         <ProgressBar ref={this.progressRef} audio={audio} />
-        <div className="media-controls__progress__time"> 99:99 </div>
+        <Duration
+          className="media-controls__progress__time"
+          audio={audio}
+          timeFormat="auto"
+          defaultDuration="--:--"
+        />
       </div>
     );
   }
