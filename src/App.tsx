938f3f8d3dd185916e0114973843eae71990c59e
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-14 10:04:02 -0600

    added test audio for testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import MainWindow from './components/MainWindow';
 import MediaControlsBar from './components/MediaControls';
 import Sidebar from './components/Sidebar';
+import testAudioFile from './assets/audio/test.mp3';
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="app">
-      <Sidebar />
-      <MediaControlsBar />
-      <MainWindow />
-    </div>
-  );
+  audio = React.createRef<HTMLAudioElement>();
+  testAudio = new Audio(testAudioFile);
+
+  render() {
+    this.testAudio.controls=false;
+    return (
+      <div className="app">
+        <Sidebar />
+        <MediaControlsBar audio={this.testAudio}/>
+        <MainWindow />
+      </div>
+    );
+  }
 }
-
 export default App;
