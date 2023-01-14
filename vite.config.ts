219e724ff21aef8b26ce9efac84a575830c2a4ba
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-13 18:25:24 -0600

    Fully converted over to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
-    plugins: [react()],
+  envPrefix: "REACT_APP_",
+  plugins: [
+    react(),
+    envCompatible(),
+    tsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
