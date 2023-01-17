0cea0a6bf15431ee0130b5bd254dff2a07a65da4
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-17 14:21:10 -0500

    added stylelint

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@ module.exports = {
     plugins: [require("prettier-plugin-tailwindcss")],
     tailwindConfig: "./tailwind.config.js",
 
+
     // ...other prettier options
     tabWidth: 4,
     semi: true,
@@ -10,6 +11,6 @@ module.exports = {
     trailingComma: "es5",
     arrowParens: "always",
     printWidth: 120,
-    tabs: false,
-    "quote-props": "as-needed",
+    useTabs: false,
+    quoteProps: "as-needed",
 };
