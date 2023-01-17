5ecd2bdbed70d1e384edbd14ce2e2f02699aaace
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-17 18:20:20 -0500

    changed icons of mute button and added different visuals depending on volume level

--- src/components/MediaControlsBar/VolumeControls/index.tsx
+++ src/components/MediaControlsBar/VolumeControls/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+import { ImVolumeMute2, ImVolumeMute, ImVolumeLow, ImVolumeMedium, ImVolumeHigh } from "react-icons/im";
 import VolumeBar from "./VolumeBar";
 import { I18nAriaLabels } from "../../../types";
 import "./VolumeControls.css";
@@ -45,17 +45,25 @@ type VolumeButtonProps = {
 
 type VolumeButtonState = {
     isMuted: boolean;
+    volume: number;
 };
 
 class VolumeButton extends Component<VolumeButtonProps, VolumeButtonState> {
     state: VolumeButtonState = {
         isMuted: false,
+        volume: 1,
     };
 
+    hasAddedAudioEventListener = false;
+
     toggleMute = (): void => {
-        const { onClick } = this.props;
+        const { onClick, audio } = this.props;
         const { isMuted } = this.state;
+        if (!audio) {
+            return;
+        }
         onClick();
+        audio.muted = !isMuted;
         this.setState({ isMuted: !isMuted });
     };
 
@@ -64,27 +72,104 @@ class VolumeButton extends Component<VolumeButtonProps, VolumeButtonState> {
         if (!audio) {
             return;
         }
-        this.setState({ isMuted: audio.muted });
+        if (audio.volume === 0) {
+            audio.muted = true;
+            this.setState({ isMuted: true });
+        }
+        else {
+            audio.muted = false;
+            this.setState({ isMuted: false });
+        }
+    };
+
+    checkVolume = (): void => {
+        const { audio } = this.props;
+        if (!audio) {
+            return;
+        }
+        this.setState({ volume: audio.volume });
+    };
+
+    addEventListenerToAudio = (): void => {
+        const { audio } = this.props;
+        if (audio && !this.hasAddedAudioEventListener) {
+            audio.addEventListener("volumechange", this.checkMuted);
+            audio.addEventListener("volumechange", this.checkVolume);
+            this.hasAddedAudioEventListener = true;
+        }
+    };
+
+    removeEventListenerFromAudio = (): void => {
+        const { audio } = this.props;
+        if (audio && this.hasAddedAudioEventListener) {
+            audio.removeEventListener("volumechange", this.checkMuted);
+            audio.removeEventListener("volumechange", this.checkVolume);
+            this.hasAddedAudioEventListener = false;
+        }
+    };
+
+    componentDidMount() {
+        this.addEventListenerToAudio();
+    }
+
+    componentDidUpdate() {
+        this.addEventListenerToAudio();
+    }
+
+    componentWillUnmount() {
+        this.removeEventListenerFromAudio();
+    }
+
+    isAudioAvailable = (): boolean => {
+        const { audio } = this.props;
+        if (!audio) {
+            return false;
+        }
+        if (audio.src === "" || audio.src === window.location.href) {
+            return false;
+        }
+        return true;
     };
 
     render() {
         const { i18nAriaLabels } = this.props;
-        const { isMuted } = this.state;
-        return (
-            <div
-                aria-label={isMuted ? i18nAriaLabels?.volumeMute : i18nAriaLabels?.volume}
-                className="media-controls-volume-button"
-                onClick={this.toggleMute}
-            >
-                {isMuted
-                    ? (
-                        <FaVolumeMute className="media-icon volume-button" />
-                    )
-                    : (
-                        <FaVolumeUp className="media-icon volume-button" />
-                    )}
-            </div>
-        );
+        const { isMuted, volume } = this.state;
+        if (!this.isAudioAvailable()) {
+            return (
+                <div className="media-controls-volume-button">
+                    <ImVolumeMute2 className="media-icon volume-button" />
+                </div>
+            );
+        }
+        else {
+            return (
+                <div
+                    aria-label={isMuted ? i18nAriaLabels?.volumeMute : i18nAriaLabels?.volume}
+                    className="media-controls-volume-button"
+                    onClick={this.toggleMute}
+                >
+                    {isMuted
+                        ? (
+                            <ImVolumeMute2 className="media-icon volume-button has-media" />
+                        )
+                        : volume < 0.25
+                            ? (
+                                <ImVolumeMute className="media-icon volume-button has-media" />
+                            )
+                            : volume < 0.5
+                                ? (
+                                    <ImVolumeLow className="media-icon volume-button has-media" />
+                                )
+                                : volume < 0.75
+                                    ? (
+                                        <ImVolumeMedium className="media-icon volume-button has-media" />
+                                    )
+                                    : (
+                                        <ImVolumeHigh className="media-icon volume-button has-media" />
+                                    )}
+                </div>
+            );
+        }
     }
 }
 
