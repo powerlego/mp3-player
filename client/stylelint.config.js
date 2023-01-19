e65eff29995087bb0c2488437d1dbb0897386a3d
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-19 16:46:46 -0500

    formatting

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: ["stylelint-config-recommended", "stylelint-config-tailwindcss", "stylelint-config-prettier"],
+  extends: ["stylelint-config-recommended", "stylelint-config-tailwindcss", "stylelint-config-prettier"],
 
-    rules: {
-        "block-no-empty": [true, { severity: "warning" }],
-        "no-duplicate-selectors": true,
-        indentation: [2, { severity: "warning" }],
-        "max-line-length": [120, { severity: "warning" }],
-    },
+  rules: {
+    "block-no-empty": [true, { severity: "warning" }],
+    "no-duplicate-selectors": true,
+    indentation: [2, { severity: "warning" }],
+    "max-line-length": [120, { severity: "warning" }],
+  },
 };
