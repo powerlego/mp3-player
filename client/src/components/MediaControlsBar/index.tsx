c7aed3b1a3f615b005e84a81d990f2561931eb4b
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-19 14:28:49 -0500

    added fetching of audio metadata

--- client/src/components/MediaControlsBar/index.tsx
+++ client/src/components/MediaControlsBar/index.tsx
@@ -3,250 +3,270 @@ import { AUDIO_PRELOAD_ATTRIBUTE, TIME_FORMAT } from "../../constants";
 import "./MediaControlsBar.css";
 import TrackProgress from "./TrackProgress";
 import MediaControls from "./MediaControls";
-import { I18nAriaLabels } from "../../types";
+import { CoverArt, I18nAriaLabels, MetaDataPayload } from "../../types";
 import VolumeControls from "./VolumeControls";
 
 type MediaControlsBarProps = {
-    src?: string;
-    progressUpdateInterval?: number;
+  src?: string;
+  progressUpdateInterval?: number;
 
-    /**
-     * HTML5 Audio tag preload property
-     */
-    preload?: AUDIO_PRELOAD_ATTRIBUTE;
-    i18nAriaLabels?: I18nAriaLabels;
-    defaultCurrentTime?: ReactNode;
-    defaultDuration?: ReactNode;
-    timeFormat?: TIME_FORMAT;
-    volume?: number;
-    muted?: boolean;
+  /**
+   * HTML5 Audio tag preload property
+   */
+  preload?: AUDIO_PRELOAD_ATTRIBUTE;
+  i18nAriaLabels?: I18nAriaLabels;
+  defaultCurrentTime?: ReactNode;
+  defaultDuration?: ReactNode;
+  timeFormat?: TIME_FORMAT;
+  volume?: number;
+  muted?: boolean;
 };
 
 type MediaControlsBarState = {
-    songName: string;
+  songName: string;
+  coverArt: CoverArt;
 };
 
 class MediaControlsBar extends React.Component<MediaControlsBarProps, MediaControlsBarState> {
-    static defaultProps: MediaControlsBarProps = {
-        timeFormat: "auto",
-        defaultCurrentTime: "--:--",
-        defaultDuration: "--:--",
-        i18nAriaLabels: {
-            player: "Audio player",
-            progressControl: "Audio progress control",
-            volumeControl: "Volume control",
-            play: "Play",
-            pause: "Pause",
-            rewind: "Rewind",
-            shuffle: "Shuffle",
-            shuffleOn: "Shuffle enabled",
-            previous: "Previous",
-            next: "Skip",
-            loop: "Disable loop",
-            loopOnce: "Enable loop once",
-            loopOff: "Enable loop",
-            volume: "Mute",
-            volumeMute: "Unmute",
-        },
-    };
+  hasFetchedMetadata = false;
 
-    state: MediaControlsBarState = {
-        songName: "",
-    };
+  static defaultProps: MediaControlsBarProps = {
+    timeFormat: "auto",
+    defaultCurrentTime: "--:--",
+    defaultDuration: "--:--",
+    i18nAriaLabels: {
+      player: "Audio player",
+      progressControl: "Audio progress control",
+      volumeControl: "Volume control",
+      play: "Play",
+      pause: "Pause",
+      rewind: "Rewind",
+      shuffle: "Shuffle",
+      shuffleOn: "Shuffle enabled",
+      previous: "Previous",
+      next: "Skip",
+      loop: "Disable loop",
+      loopOnce: "Enable loop once",
+      loopOff: "Enable loop",
+      volume: "Mute",
+      volumeMute: "Unmute",
+    },
+  };
 
-    audio = React.createRef<HTMLAudioElement>();
+  state: MediaControlsBarState = {
+    songName: "",
+    coverArt: {
+      data: "",
+      format: "",
+    },
+  };
 
-    lastVolume = this.props.volume ? this.props.volume : 1;
+  audio = React.createRef<HTMLAudioElement>();
 
-    togglePlay = (e: React.SyntheticEvent): void => {
-        e.stopPropagation();
-        const audio = this.audio.current;
-        if (!audio) {
-            return;
-        }
-        if ((audio.paused || audio.ended) && audio.src) {
-            this.playAudioPromise();
-        }
-        else if (!audio.paused) {
-            audio.pause();
-        }
-    };
+  lastVolume = this.props.volume ? this.props.volume : 1;
 
-    handleClick = () => {
-        const { src } = this.props;
-        if (!src) {
-            return;
-        }
-        if (!import.meta.env.REACT_APP_SERVER_URL) {
-            console.log("No server URL provided");
-            return;
-        }
-        const url = new URL(import.meta.env.REACT_APP_SERVER_URL as string);
-        fetch(src)
-            .then(async (res) => {
-                if (res.ok) {
-                    if (res.body) {
-                        url.pathname = "/metadata";
-                        const resText = await res.blob();
-                        const form = new FormData();
-                        form.append("track", resText);
-                        const resp = await fetch(url, {
-                            method: "POST",
-                            body: form,
-                        });
-                        const respText = await resp.text();
-                        console.log(respText);
-                    }
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-    playAudioPromise = (): void => {
-        const audio = this.audio.current;
-        if (!audio) {
-            return;
-        }
-        const playPromise = audio.play();
-        // playPromise is null in IE 11
+  togglePlay = (e: React.SyntheticEvent): void => {
+    e.stopPropagation();
+    const audio = this.audio.current;
+    if (!audio) {
+      return;
+    }
+    if ((audio.paused || audio.ended) && audio.src) {
+      this.playAudioPromise();
+    }
+    else if (!audio.paused) {
+      audio.pause();
+    }
+  };
 
-        playPromise.then(null).catch((err) => {
-            console.log(err);
+  playAudioPromise = (): void => {
+    const audio = this.audio.current;
+    if (!audio) {
+      return;
+    }
+    const playPromise = audio.play();
+    // playPromise is null in IE 11
+
+    playPromise.then(null).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handlePlay = (e: Event): void => {
+    this.forceUpdate();
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handlePause = (e: Event): void => {
+    if (!this.audio) {
+      return;
+    }
+    this.forceUpdate();
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleEnded = (e: Event): void => {
+    if (!this.audio) {
+      return;
+    }
+    // Remove forceUpdate when stop supporting IE 11
+    this.forceUpdate();
+  };
+
+  handleClickVolumeButton = (): void => {
+    const audio = this.audio.current;
+    if (!audio) {
+      return;
+    }
+    if (audio.volume > 0) {
+      this.lastVolume = audio.volume;
+      audio.volume = 0;
+    }
+    else {
+      audio.volume = this.lastVolume;
+    }
+  };
+
+  handleMuteChange = (): void => {
+    this.forceUpdate();
+  };
+
+  handleClickLoopButton = (): void => {
+    if (!this.audio.current) {
+      return;
+    }
+    this.audio.current.loop = !this.audio.current.loop;
+    this.forceUpdate();
+  };
+
+  fetchMetadata = (): void => {
+    const audio = this.audio.current;
+    if (!audio) {
+      return;
+    }
+    const { src } = audio;
+    if (!this.hasFetchedMetadata) {
+      this.hasFetchedMetadata = true;
+      if (!import.meta.env.REACT_APP_SERVER_URL) {
+        console.log("No server URL provided");
+        return;
+      }
+      const url = new URL(import.meta.env.REACT_APP_SERVER_URL as string);
+      fetch(src)
+        .then(async (res) => {
+          if (res.ok) {
+            if (res.body) {
+              url.pathname = "/metadata";
+              const resText = await res.blob();
+              const form = new FormData();
+              form.append("track", resText);
+              const resp = await fetch(url, {
+                method: "POST",
+                body: form,
+              });
+              const payload = (await resp.json()) as MetaDataPayload;
+              const { name, cover } = payload.data;
+              this.setState({
+                songName: name,
+                coverArt: cover,
+              });
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(err);
         });
-    };
+    }
+  };
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    handlePlay = (e: Event): void => {
-        this.forceUpdate();
-    };
+  componentDidMount() {
+    this.forceUpdate();
+    const audio = this.audio.current;
+    if (!audio) {
+      return;
+    }
+    if (this.props.muted) {
+      audio.volume = 0;
+    }
+    else {
+      audio.volume = this.lastVolume;
+    }
+    audio.addEventListener("loadstart", this.fetchMetadata);
+    // mmb.fetchFromUrl(audio.src)
+    //     .then((metadata) => {
+    //         console.log(metadata);
+    //     })
+    //     .finally(() => {
+    //         this.setState({
+    //             songName: "Song Name",
+    //         });
+    //     });
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    handlePause = (e: Event): void => {
-        if (!this.audio) {
-            return;
-        }
-        this.forceUpdate();
-    };
+    // When audio play starts
+    audio.addEventListener("play", this.handlePlay);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    handleEnded = (e: Event): void => {
-        if (!this.audio) {
-            return;
-        }
-        // Remove forceUpdate when stop supporting IE 11
-        this.forceUpdate();
-    };
+    audio.addEventListener("ended", this.handleEnded);
 
-    handleClickVolumeButton = (): void => {
-        const audio = this.audio.current;
-        if (!audio) {
-            return;
-        }
-        if (audio.volume > 0) {
-            this.lastVolume = audio.volume;
-            audio.volume = 0;
-        }
-        else {
-            audio.volume = this.lastVolume;
-        }
-    };
+    // When the user pauses playback
+    audio.addEventListener("pause", this.handlePause);
+  }
 
-    handleMuteChange = (): void => {
-        this.forceUpdate();
-    };
-
-    handleClickLoopButton = (): void => {
-        if (!this.audio.current) {
-            return;
-        }
-        this.audio.current.loop = !this.audio.current.loop;
-        this.forceUpdate();
-    };
-
-    componentDidMount() {
-        this.forceUpdate();
-        const audio = this.audio.current;
-        if (!audio) {
-            return;
-        }
-        if (this.props.muted) {
-            audio.volume = 0;
-        }
-        else {
-            audio.volume = this.lastVolume;
-        }
-        // mmb.fetchFromUrl(audio.src)
-        //     .then((metadata) => {
-        //         console.log(metadata);
-        //     })
-        //     .finally(() => {
-        //         this.setState({
-        //             songName: "Song Name",
-        //         });
-        //     });
-
-        // When audio play starts
-        audio.addEventListener("play", this.handlePlay);
-
-        audio.addEventListener("ended", this.handleEnded);
-
-        // When the user pauses playback
-        audio.addEventListener("pause", this.handlePause);
+  componentDidUpdate(prevProps: MediaControlsBarProps): void {
+    const { src } = this.props;
+    if (prevProps.src !== src) {
+      // if (autoPlayAfterSrcChange) {
+      //     this.playAudioPromise();
+      // }
+      // else {
+      // Updating pause icon to play icon
+      this.forceUpdate();
+      // }
+    }
+  }
+  isPlaying = (): boolean => {
+    const audio = this.audio.current;
+    if (!audio) {
+      return false;
     }
 
-    componentDidUpdate(prevProps: MediaControlsBarProps): void {
-        const { src } = this.props;
-        if (prevProps.src !== src) {
-            // if (autoPlayAfterSrcChange) {
-            //     this.playAudioPromise();
-            // }
-            // else {
-            // Updating pause icon to play icon
-            this.forceUpdate();
-            // }
-        }
-    }
-    isPlaying = (): boolean => {
-        const audio = this.audio.current;
-        if (!audio) {
-            return false;
-        }
+    return !audio.paused && !audio.ended;
+  };
 
-        return !audio.paused && !audio.ended;
-    };
-
-    render() {
-        const { src, timeFormat, defaultCurrentTime, defaultDuration, i18nAriaLabels } = this.props;
-        const audio = this.audio.current;
-        if (!timeFormat) {
-            return null;
-        }
-        return (
-            <div aria-label={i18nAriaLabels?.player} className="media-container">
-                <audio controls={false} ref={this.audio} src={src} />
-                <div className="media-controls">
-                    <div className="m-0 flex h-full w-56 bg-gray-200" />
-                    <div className="media-controls-center">
-                        <MediaControls audio={audio} i18nAriaLabels={i18nAriaLabels} togglePlay={this.togglePlay} />
-                        <TrackProgress
-                            audio={audio}
-                            defaultCurrentTime={defaultCurrentTime}
-                            defaultDuration={defaultDuration}
-                            i18nAriaLabels={i18nAriaLabels}
-                            timeFormat={timeFormat}
-                        />
-                    </div>
-                    <VolumeControls
-                        audio={audio}
-                        i18nAriaLabels={i18nAriaLabels}
-                        onClickedVolumeButton={this.handleClickVolumeButton}
-                        onMuteChange={this.handleMuteChange}
-                    />
-                </div>
-            </div>
-        );
+  render() {
+    const { src, timeFormat, defaultCurrentTime, defaultDuration, i18nAriaLabels } = this.props;
+    const { coverArt } = this.state;
+    const audio = this.audio.current;
+    if (!timeFormat) {
+      return null;
     }
+    return (
+      <div aria-label={i18nAriaLabels?.player} className="media-container">
+        <audio controls={false} ref={this.audio} src={src} />
+        <div className="media-controls">
+          <div className="m-0 flex items-center h-full w-auto min-w-56 bg-gray-200">
+            <img className="h-1/2 aspect-square" src={`data:${coverArt.format};base64,${coverArt.data}`} />
+          </div>
+          <div className="media-controls-center">
+            <MediaControls audio={audio} i18nAriaLabels={i18nAriaLabels} togglePlay={this.togglePlay} />
+            <TrackProgress
+              audio={audio}
+              defaultCurrentTime={defaultCurrentTime}
+              defaultDuration={defaultDuration}
+              i18nAriaLabels={i18nAriaLabels}
+              timeFormat={timeFormat}
+            />
+          </div>
+          <VolumeControls
+            audio={audio}
+            i18nAriaLabels={i18nAriaLabels}
+            onClickedVolumeButton={this.handleClickVolumeButton}
+            onMuteChange={this.handleMuteChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MediaControlsBar;
