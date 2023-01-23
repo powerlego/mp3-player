d8ddf47f74fba6058f944ed89d069529cbbcf241
powerlego <11838540+powerlego@users.noreply.github.com>
2023-01-23 13:13:10 -0500

    Moved song name details to own component

---
+++ client/src/components/MediaControlsBar/SongDetails/SongName/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { IterationType, ScrollingAnimationProps } from "../../../../types";
+import useScrollingAnimation from "../../../../hooks/useScrollingAnimation";
+import "./SongName.css";
+
+interface SongNameProps {
+  songName: string;
+  animationProps?: ScrollingAnimationProps;
+}
+
+export default function SongName(props: SongNameProps) {
+  const {
+    songName,
+    animationProps = {
+      speed: 0.2,
+      pauseAtEndEdgeDurationMs: 1200,
+      initialMouseIntDelayMs: 200,
+      iterationType: IterationType.single,
+    },
+  } = props;
+
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const offsetRef = React.useRef<HTMLDivElement>(null);
+  const [handleMouseOver, handleMouseOut] = useScrollingAnimation({ containerRef, offsetRef, animationProps });
+
+  return (
+    <div
+      className="song-details-name-align"
+      onBlur={() => handleMouseOut}
+      onMouseOut={() => handleMouseOut}
+      onFocus={() => {
+        handleMouseOver(true);
+      }}
+      onMouseOver={() => {
+        handleMouseOver(false);
+      }}
+    >
+      <div className="song-details-name-container" ref={containerRef}>
+        <div className="song-details-name-offset" ref={offsetRef}>
+          <span className="song-details-name">{songName}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
