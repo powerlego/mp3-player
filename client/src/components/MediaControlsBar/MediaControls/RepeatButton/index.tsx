2bff44558617d457fb17d1b39f420710efb4fda0
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2023-01-31 14:54:08 -0500

    added test id selectors

--- client/src/components/MediaControlsBar/MediaControls/RepeatButton/index.tsx
+++ client/src/components/MediaControlsBar/MediaControls/RepeatButton/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { ReactComponent as LoopIcon } from "../../../../assets/icons/loop.svg";
 import { ReactComponent as LoopOnceIcon } from "../../../../assets/icons/loop_once.svg";
-import "./RepeatButton.css";
 
 export default function RepeatButton() {
   const [repeat, setRepeat] = useState(0);
@@ -14,17 +13,33 @@ export default function RepeatButton() {
     }
   };
   return (
-    <div className="media-icon-container" onClick={() => handleRepeat()}>
+    <div
+      className="h-8 aspect-square flex justify-center items-center"
+      data-testid="repeat-button"
+      onClick={() => handleRepeat()}
+    >
       {repeat === 1
         ? (
-          <LoopIcon className="media-icon repeat-button repeating" />
+          <LoopIcon
+            className="m-0 w-4 h-4 cursor-pointer transition-all duration-300 ease-in-out fill-green-450 hover:fill-green-500 dark:fill-green-350 hover:dark:fill-green-450"
+            data-testid="repeat-icon"
+            id="repeat-all"
+          />
         )
         : repeat === 2
           ? (
-            <LoopOnceIcon className="media-icon repeat-button repeating" />
+            <LoopOnceIcon
+              className="m-0 w-4 h-4 cursor-pointer transition-all duration-300 ease-in-out fill-green-450 hover:fill-green-500 dark:fill-green-350 hover:dark:fill-green-450"
+              data-testid="repeat-icon"
+              id="repeat-one"
+            />
           )
           : (
-            <LoopIcon className="media-icon repeat-button" />
+            <LoopIcon
+              className="m-0 w-4 h-4 cursor-pointer fill-gray-450 transition-all duration-300 ease-in-out hover:fill-gray-550 dark:fill-gray-550 hover:dark:fill-gray-150"
+              data-testid="repeat-icon"
+              id="repeat-none"
+            />
           )}
     </div>
   );
