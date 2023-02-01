73a268a50df1371611c34fe7ee0dcef2195a61ac
powerlego <11838540+powerlego@users.noreply.github.com>
2023-01-31 19:24:02 -0500

    Changed from class component to functional component

--- client/src/components/MediaControlsBar/MediaControls/index.tsx
+++ client/src/components/MediaControlsBar/MediaControls/index.tsx
@@ -12,21 +12,18 @@ interface MediaControlsProps {
   i18nAriaLabels?: I18nAriaLabels;
 }
 
-export default class MediaControls extends React.Component<MediaControlsProps> {
-  render() {
-    const { audio, togglePlay, i18nAriaLabels } = this.props;
-    return (
-      <div className="mb-4 flex flex-row items-center justify-evenly gap-4 w-full">
-        <div className="flex items-center justify-end w-full flex-1 gap-2">
-          <ShuffleButton audio={audio} />
-          <SkipBackButton aria-label={i18nAriaLabels?.previous} />
-        </div>
-        <PlayButton audio={audio} i18nAriaLabels={i18nAriaLabels} togglePlay={togglePlay} />
-        <div className="flex items-center justify-start w-full flex-1 gap-2">
-          <SkipForwardButton aria-label={i18nAriaLabels?.next} />
-          <RepeatButton />
-        </div>
+export default function MediaControls({ audio, togglePlay, i18nAriaLabels }: MediaControlsProps) {
+  return (
+    <div className="mb-4 flex flex-row items-center justify-evenly gap-4 w-full">
+      <div className="flex items-center justify-end w-full flex-1 gap-2">
+        <ShuffleButton audio={audio} />
+        <SkipBackButton aria-label={i18nAriaLabels?.previous} />
       </div>
-    );
-  }
+      <PlayButton audio={audio} i18nAriaLabels={i18nAriaLabels} togglePlay={togglePlay} />
+      <div className="flex items-center justify-start w-full flex-1 gap-2">
+        <SkipForwardButton aria-label={i18nAriaLabels?.next} />
+        <RepeatButton />
+      </div>
+    </div>
+  );
 }
