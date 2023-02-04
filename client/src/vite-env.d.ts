858021037d7c8e3d1857938758dfbf40639ac3a3
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-03 19:39:12 -0500

    changed mp3 declare module to be specific

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="node/url"/>
-declare module "*.mp3";
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
