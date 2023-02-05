8d7e25c0be0cb8f28b1087fb1ea014f0d398088f
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-05 14:29:37 -0500

    added progressUpdateInterval props

--- src/components/MediaControlsBar/TrackProgress/TrackProgress.tsx
+++ src/components/MediaControlsBar/TrackProgress/TrackProgress.tsx
@@ -7,13 +7,21 @@ import ProgressBar from "./ProgressBar";
 
 interface TrackProgressProps {
   audio: HTMLAudioElement | null;
+  progressUpdateInterval?: number;
   defaultCurrentTime?: ReactNode;
   defaultDuration?: ReactNode;
   timeFormat?: TIME_FORMAT;
   i18nAriaLabels?: I18nAriaLabels;
 }
 
-function TrackProgress({ audio, timeFormat, defaultCurrentTime, defaultDuration, i18nAriaLabels }: TrackProgressProps) {
+function TrackProgress({
+  audio,
+  progressUpdateInterval,
+  timeFormat,
+  defaultCurrentTime,
+  defaultDuration,
+  i18nAriaLabels,
+}: TrackProgressProps) {
   const progressRef = React.useRef<HTMLDivElement>(null);
   if (!timeFormat) {
     return null;
@@ -26,7 +34,12 @@ function TrackProgress({ audio, timeFormat, defaultCurrentTime, defaultDuration,
         defaultCurrentTime={defaultCurrentTime}
         timeFormat={timeFormat}
       />
-      <ProgressBar audio={audio} i18nProgressBar={i18nAriaLabels?.progressControl} ref={progressRef} />
+      <ProgressBar
+        audio={audio}
+        i18nProgressBar={i18nAriaLabels?.progressControl}
+        progressUpdateInterval={progressUpdateInterval}
+        ref={progressRef}
+      />
       <Duration
         audio={audio}
         className="min-w-[2.5rem] text-xs text-gray-700 dark:text-gray-350 text-left"
