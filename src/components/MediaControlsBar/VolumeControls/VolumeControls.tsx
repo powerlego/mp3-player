ec5d7ab2bc3d69122a99e8f7a38bdad2e1c47338
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-05 13:51:32 -0500

    Made volume controls functional component

--- src/components/MediaControlsBar/VolumeControls/VolumeControls.tsx
+++ src/components/MediaControlsBar/VolumeControls/VolumeControls.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import VolumeBar from "./VolumeBar";
 import { I18nAriaLabels } from "@/types";
@@ -6,35 +6,22 @@ import VolumeButton from "./VolumeButton";
 
 interface VolumeControlsProps {
   audio: HTMLAudioElement | null;
-  onMuteChange: () => void;
   onClickedVolumeButton: () => void;
   i18nAriaLabels?: I18nAriaLabels;
 }
 
-interface VolumeControlsState {
-  volume: number;
-}
-
-class VolumeControls extends Component<VolumeControlsProps, VolumeControlsState> {
-  state: VolumeControlsState = {
-    volume: 1,
-  };
-
-  render() {
-    const { audio, i18nAriaLabels, onClickedVolumeButton } = this.props;
-    const { volume } = this.state;
-    return (
-      <div
-        className="flex items-center"
-        style={{
-          flex: "0 1 7.75rem",
-        }}
-      >
-        <VolumeButton audio={audio} i18nAriaLabels={i18nAriaLabels} onClick={onClickedVolumeButton} />
-        <VolumeBar audio={audio} i18nVolumeControl={i18nAriaLabels?.volumeControl} initVolume={volume} />
-      </div>
-    );
-  }
+function VolumeControls({ audio, onClickedVolumeButton, i18nAriaLabels }: VolumeControlsProps): JSX.Element {
+  return (
+    <div
+      className="flex items-center"
+      style={{
+        flex: "0 1 7.75rem",
+      }}
+    >
+      <VolumeButton audio={audio} i18nAriaLabels={i18nAriaLabels} onClick={onClickedVolumeButton} />
+      <VolumeBar audio={audio} i18nVolumeControl={i18nAriaLabels?.volumeControl} initVolume={1} />
+    </div>
+  );
 }
 
 export default VolumeControls;
