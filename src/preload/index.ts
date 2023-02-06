474df09eba9a432e52c48a234f4b94939dd94a27
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-06 16:35:51 -0500

    added space to add custom title bar

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -5,22 +5,10 @@
 import { contextBridge, ipcRenderer } from "electron";
 import { electronAPI } from "@electron-toolkit/preload";
 
+window.addEventListener("DOMContentLoaded", () => {});
+
 // Custom APIs for renderer
 const api = {
-  send: (channel: string, data: any) => {
-    // whitelist channels
-    const validChannels = ["toMain"];
-    if (validChannels.includes(channel)) {
-      ipcRenderer.send(channel, data);
-    }
-  },
-  receive: (channel: string, func: any) => {
-    const validChannels = ["fromMain"];
-    if (validChannels.includes(channel)) {
-      // Deliberately strip event as it includes `sender`
-      ipcRenderer.on(channel, (event, ...args) => func(...args));
-    }
-  },
   getAudioFile: () => ipcRenderer.invoke("getAudioFile"),
   getStoreKey: (key: string) => ipcRenderer.invoke("getStoreKey", key),
   setStoreKey: (key: string, value: any) => ipcRenderer.invoke("setStoreKey", key, value),
