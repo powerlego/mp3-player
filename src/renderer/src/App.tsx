f5f61472f0f998b33e65568141a7b9144c7d71eb
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-07 17:29:57 -0500

    moved class attribute definitions to parent

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,6 +3,7 @@ import MainWindow from "./components/MainWindow";
 import MediaControlsBar from "./components/MediaControlsBar";
 import Sidebar from "./components/Sidebar";
 import testAudioFile from "@/assets/audio/Hymn-For-The-Weekend.mp3";
+import Topbar from "./components/Topbar";
 
 function App() {
   const [expanded, setExpanded] = React.useState(false);
@@ -14,15 +15,16 @@ function App() {
   };
 
   return (
-    <div
-      className="relative grid overflow-y-hidden h-screen min-h-screen w-full"
-      style={{
-        gridTemplateColumns: "auto 1fr",
-      }}
-    >
-      <Sidebar />
-      <MediaControlsBar expandFunc={expandSongDetails} src={audio.src} />
-      <MainWindow />
+    <div className="relative grid overflow-y-hidden h-full min-h-full w-full grid-areas-layout grid-rows-layout grid-cols-layout">
+      <Topbar className="grid-in-main-view z-10 m-0 h-16 w-full flex flex-row items-center justify-evenly" />
+      <Sidebar className="z-20 m-0 w-56 grid-in-nav-bar bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-white" />
+      <MediaControlsBar
+        expandFunc={expandSongDetails}
+        src={audio.src}
+        className="grid-in-now-playing z-30 h-[90px] w-full bg-gray-350 dark:bg-gray-750 px-4"
+      />
+      <MainWindow className="grid-in-main-view flex flex-col overflow-hidden w-full bg-gray-300 dark:bg-gray-700" />
+      <div className="bottom-[108px] left-1/2 absolute -translate-x-1/2 z-[100]" />
     </div>
     // {/* </div> */}
   );
