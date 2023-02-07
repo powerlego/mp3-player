62054ecca020bc830f01b17339477b817c7a157c
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-07 18:09:13 -0500

    added better placeholders that simulate scrolling

--- src/renderer/src/components/MainWindow/MainWindow.tsx
+++ src/renderer/src/components/MainWindow/MainWindow.tsx
@@ -5,12 +5,22 @@ type MainWindowProps = {
 };
 
 function MainWindow({ className }: MainWindowProps) {
+  const arr: string[] = [];
+  for (let i = 0; i < 100; i++) {
+    arr.push(
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum asperiores harum minima nemo sunt deserunt, error sint recusandae modi, repellendus impedit officiis eius necessitatibus atque sed esse rem assumenda illum!"
+    );
+  }
+
   return (
     <div className={className}>
       <div className="h-16 sticky top-0" />
       <div className="h-full w-full flex flex-col items-start justify-start overflow-y-scroll">
-        <p className="w-full text-center text-gray-800 dark:text-white">Hello</p>
-        
+        {arr.map((item, index) => (
+          <p key={index} className="w-full text-gray-800 dark:text-white">
+            {item}
+          </p>
+        ))}
       </div>
     </div>
   );
