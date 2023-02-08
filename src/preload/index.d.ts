28db2bb00495805950fcfd85cc8ba51227ab5801
powerlego <11838540+powerlego@users.noreply.github.com>
2023-02-08 00:32:44 -0500

    added types for callback

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,23 @@
 import { ElectronAPI } from "@electron-toolkit/preload";
-
+import { IAudioMetadata } from "music-metadata/lib/type";
 declare global {
   interface Window {
     electron: ElectronAPI;
-    api: unknown;
+    api: {
+      onFileOpen: (
+        callback: (
+          _event: any,
+          file: {
+            fileName: string;
+            metadata: IAudioMetadata;
+            uint8Array: Uint8Array;
+          }
+        ) => void
+      ) => Electron.IpcRenderer;
+      getAudioFile: () => Promise<string>;
+      loadAudioFile: () => Promise<string>;
+      getStoreKey: (key: string) => Promise<any>;
+      setStoreKey: (key: string, value: any) => Promise<any>;
+    };
   }
 }
