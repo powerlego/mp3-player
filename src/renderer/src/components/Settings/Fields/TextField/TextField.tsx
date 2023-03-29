f7b6db188078b9174c8647bb37bed5204fbe04df
powerlego <11838540+powerlego@users.noreply.github.com>
2023-03-29 12:57:45 -0400

    Finished implementation of TextField

--- src/renderer/src/components/Settings/Fields/TextField/TextField.tsx
+++ src/renderer/src/components/Settings/Fields/TextField/TextField.tsx
@@ -2,16 +2,26 @@ import { SettingsItem } from "@/types";
 import React from "react";
 
 type TextFieldProps = {
+  className?: string;
   field: SettingsItem;
   value: string;
-  onChange: (value: string) => void;
+  onChange: (key: string, value: any) => void;
 };
 
-export default function TextField({ field, value, onChange }: TextFieldProps) {
+export default function TextField({ className, field, value, onChange }: TextFieldProps) {
   return (
-    <div className={`key-${field.key}`}>
-      <div>{field.label}</div>
-      <input aria-label={field.label} type="text" value={value} onChange={(e) => onChange(e.target.value)} />;
+    <div className={`${className ?? ""} field-text key-${field.key} `}>
+      <div className={"font-bold mb-3 text-base"}>{field.label}</div>
+      <input
+        aria-label={field.label}
+        style={{ outline: "none" }}
+        type="text"
+        value={value}
+        className={
+          "w-full rounded-lg text-sm m-[1px] p-2 bg-gray-50 dark:bg-gray-650 border border-solid border-gray-350 dark:border-gray-550 transition-[border-color] duration-300 ease-out focus:m-0 focus:border-blue-200 dark:focus:border-blue-300 focus:border-2"
+        }
+        onChange={(e) => onChange(field.key, e.target.value)}
+      />
     </div>
   );
 }
