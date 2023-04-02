630b194f955ad58709eb9baa522cd5f1073d91e1
powerlego <11838540+powerlego@users.noreply.github.com>
2023-04-02 12:07:05 -0400

    made debounce typescript compatible

--- src/renderer/src/utils/debounce.ts
+++ src/renderer/src/utils/debounce.ts
@@ -1,6 +1,6 @@
 function debounce(func: (...args: any) => any, wait: number) {
-  let timeout: NodeJS.Timeout | number;
-  return (...args: any) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
     clearTimeout(timeout);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     timeout = setTimeout(() => func.apply(this, args), wait);
