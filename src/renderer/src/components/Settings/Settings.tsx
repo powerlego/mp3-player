dfb1d73a09ab480d0ab119f128b33986768d4d0a
powerlego <11838540+powerlego@users.noreply.github.com>
2023-04-05 14:28:51 -0400

    Moved settings buttons to SettingsWindow for proper use

--- src/renderer/src/components/Settings/Settings.tsx
+++ src/renderer/src/components/Settings/Settings.tsx
@@ -1,30 +1,11 @@
 import React from "react";
 import useDarkMode from "@renderer/hooks/useDarkMode";
-import SettingsButton from "@renderer/components/Settings/SettingsButton/";
+
 import SettingsWindow from "./SettingsWindow";
 
 export default function Settings(): JSX.Element {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const [darkTheme, setDarkTheme] = useDarkMode();
 
-  return (
-    <>
-      <div className="absolute top-0 left-0 right-0 bottom-[4.5rem] bg-gray-180 dark:bg-gray-860 text-gray-800 dark:text-white flex flex-col">
-        <SettingsWindow />
-      </div>
-      <div className="absolute bottom-8 left-0 right-0 h-10 bg-gray-220 dark:bg-gray-880 text-gray-800 dark:text-white flex flex-row justify-end items-center">
-        <div className="flex flex-row justify-between gap-2 mr-2">
-          <SettingsButton onClick={() => window.close()}>
-            <span className="text-sm">Cancel</span>
-          </SettingsButton>
-          <SettingsButton onClick={() => console.log(window.settings.getSections())}>
-            <span className="text-sm">Apply</span>
-          </SettingsButton>
-          <SettingsButton>
-            <span className="text-sm">Save & Close</span>
-          </SettingsButton>
-        </div>
-      </div>
-    </>
-  );
+  return <SettingsWindow />;
 }
