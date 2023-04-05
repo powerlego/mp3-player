556b2a019973d15b7cc3c159f792946091884911
powerlego <11838540+powerlego@users.noreply.github.com>
2023-04-05 14:28:18 -0400

    made debounce return async function

--- src/renderer/src/utils/debounce.ts
+++ src/renderer/src/utils/debounce.ts
@@ -1,9 +1,20 @@
 function debounce(func: (...args: any) => any, wait: number) {
   let timeout: ReturnType<typeof setTimeout>;
-  return function (this: any, ...args: any[]) {
+  let reject: (reason?: any) => void;
+  return async function (this: any, ...args: any[]) {
+    if (timeout) {
+      reject();
+    }
     clearTimeout(timeout);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    timeout = setTimeout(() => func.apply(this, args), wait);
+    const promise = new Promise((resolve: (value: void) => void, _reject) => {
+      reject = _reject;
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+        resolve();
+      }, wait);
+    });
+    return promise;
   };
 }
 
