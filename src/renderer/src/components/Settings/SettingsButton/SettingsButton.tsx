59b7ad259eb3a81cc198198bf328a1d11d3fff41
powerlego <11838540+powerlego@users.noreply.github.com>
2023-04-18 13:20:55 -0400

    Fixed styling

--- src/renderer/src/components/Settings/SettingsButton/SettingsButton.tsx
+++ src/renderer/src/components/Settings/SettingsButton/SettingsButton.tsx
@@ -8,7 +8,7 @@ type SettingsButtonProps = PropsWithChildren<{
 export default function SettingsButton({ enabled = true, onClick, children }: SettingsButtonProps): JSX.Element {
   return (
     <button
-      className="w-[6.5rem] bg-gray-50 dark:bg-gray-800 px-2 rounded-md py-1 enabled:hover:bg-gray-100 dark:enabled:hover:bg-gray-700 transition ease-in-out disabled:bg-gray-100 dark:disabled:bg-gray-850 disabled:cursor-not-allowed"
+      className="w-[6.5rem] bg-gray-50 dark:bg-gray-800 px-2 rounded-md py-1 enabled:hover:bg-gray-100 dark:enabled:hover:bg-gray-700 transition ease-in-out disabled:bg-gray-100 dark:disabled:bg-gray-850 enabled:active:bg-gray-150 dark:enabled:active:bg-gray-650 disabled:cursor-not-allowed"
       disabled={!enabled}
       onClick={onClick}
     >
