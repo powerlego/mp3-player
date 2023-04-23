d3ff02f326b8b4be61c1f1d2c41e822398ab19e1
powerlego <11838540+powerlego@users.noreply.github.com>
2023-04-23 15:41:26 -0400

    Create queue for songs Fixes #28

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Queue, QueueObject } from "./objects/QueueObject";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Queue.Provider value={new QueueObject()}>
+        <App />
+      </Queue.Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
