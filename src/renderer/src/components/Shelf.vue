e793b36e6ff57543da5aa204d45e650708bf9ad3
Nicholas Curl <nicholas_curl@na.honda.com>
2024-08-22 12:30:58 -0400

    Begun converting shelf component to v2 implementation

---
+++ src/renderer/src/components/Shelf.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, reactive, ref, watchEffect } from "vue";
+import { getColumns } from "@renderer/constants";
+
+
+const props = defineProps<{
+  title?: string;
+}>();
+
+const defaultColGap = 24;
+const columns = reactive({
+  count: -1,
+  width: -1,
+  minContainerWidth: -1,
+  gap: defaultColGap,
+});
+const columnConfig = ref(getColumns("default"));
+const container = ref<HTMLElement | null>(null);
+const width = ref(0);
+
+watchEffect(() => {
+  width.value = container.value?.offsetWidth ?? 0;
+});
+
+</script>
