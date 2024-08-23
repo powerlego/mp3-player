ce22d3ce9e7dea96ac47095cedeb6ac7a3df4bdf
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-23 09:04:42 -0400

    Added setter for audio store

--- src/renderer/src/stores/audio.ts
+++ src/renderer/src/stores/audio.ts
@@ -3,7 +3,11 @@ import { ref } from "vue";
 
 export const useAudio = defineStore("audio", () => {
   const audio = ref<HTMLAudioElement | null>(null);
+  function setAudio(audioElement: HTMLAudioElement) {
+    audio.value = audioElement;
+  }
   return {
     audio,
+    setAudio,
   };
 });
