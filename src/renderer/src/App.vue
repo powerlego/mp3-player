a876011c7325d7f5665a5bcb7fac1b0dd590d887
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-23 09:06:26 -0400

    Begun to convert App component to v2

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+import { FilePayload } from "@/types";
+import log from "electron-log/renderer";
+import MainWindow from "@components/MainWindow.vue";
+import { storeToRefs } from "pinia";
+import Topbar from "@components/Topbar.vue";
+import { useAudio } from "@stores/audio";
+
+const audioStore = useAudio();
+const { audio } = storeToRefs(audioStore);
+const { setAudio } = audioStore;
+const audioElement = ref<HTMLAudioElement | null>(null);
+const audioSrc = ref("");
+
+function playAudio() {
+  if (audio.value) {
+    if (audioSrc.value !== "") {
+      audio.value.play().catch((error) => {
+        log.error("Failed to play audio", error);
+      });
+    }
+    else {
+      log.error("Audio source is empty");
+    }
+  }
+}
+
+function handleFileOpen(_event: Electron.IpcRendererEvent, file: FilePayload, play: boolean) {
+  audioSrc.value = URL.createObjectURL(new Blob([file.uint8Array], { type: "audio/mpeg" }));
+  if (play) {
+    playAudio();
+  }
+}
+
+onMounted(() => {
+  setAudio(audioElement.value!);
+  window.api.on("open-file", handleFileOpen);
+});
+
+onUnmounted(() => {
+  window.api.off("open-file", handleFileOpen);
+});
+
+</script>
+
+<template>
+  <audio
+    ref="audioElement"
+    :src="audioSrc"
+    preload="none"
+    :controls="false"
+  />
+  <div
+    class="relative grid w-full h-full min-h-full overflow-y-hidden grid-areas-layout grid-rows-layout grid-cols-layout"
+  >
+    <Topbar class="z-10 flex flex-row items-center w-full h-16 m-0 grid-in-main-view justify-evenly" />
+    <MainWindow class="flex flex-col w-full overflow-hidden grid-in-main-view bg-gray-180 dark:bg-gray-860" />
+  </div>
+</template>
