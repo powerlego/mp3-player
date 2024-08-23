75e012f235ce80c9bb88a35eb1285bb19409f3ea
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-23 09:09:21 -0400

    Added sidebar component to App component

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -3,6 +3,7 @@ import { onMounted, onUnmounted, ref } from "vue";
 import { FilePayload } from "@/types";
 import log from "electron-log/renderer";
 import MainWindow from "@components/MainWindow.vue";
+import Sidebar from "@components/Sidebar.vue";
 import { storeToRefs } from "pinia";
 import Topbar from "@components/Topbar.vue";
 import { useAudio } from "@stores/audio";
@@ -55,6 +56,7 @@ onUnmounted(() => {
     class="relative grid w-full h-full min-h-full overflow-y-hidden grid-areas-layout grid-rows-layout grid-cols-layout"
   >
     <Topbar class="z-10 flex flex-row items-center w-full h-16 m-0 grid-in-main-view justify-evenly" />
+    <Sidebar class="z-20 w-56 m-0 text-gray-800 grid-in-nav-bar bg-gray-150 dark:bg-gray-900 dark:text-white" />
     <MainWindow class="flex flex-col w-full overflow-hidden grid-in-main-view bg-gray-180 dark:bg-gray-860" />
   </div>
 </template>
