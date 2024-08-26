263866d8bd6bc0a59229a1bd177a8310e99ae357
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-25 20:54:28 -0400

    Changed icon size

--- src/renderer/src/components/PlayButton.vue
+++ src/renderer/src/components/PlayButton.vue
@@ -11,22 +11,22 @@ const { togglePlay } = useAudio();
 <template>
   <div
     v-if="isAudioAvailable"
-    class="h-12 aspect-square flex justify-center items-center rounded-full transition-all ease-in-out duration-200 hover:scale-[1.1] bg-gray-850 dark:bg-gray-150 hover:bg-gray-800 dark:hover:bg-gray-200"
+    class="h-8 aspect-square flex justify-center items-center rounded-full transition-all ease-in-out duration-200 hover:scale-[1.1] bg-gray-850 dark:bg-gray-150 hover:bg-gray-800 dark:hover:bg-gray-200"
     @click="togglePlay"
   >
     <PlayIcon
       v-if="!isPlaying"
-      class="w-12 h-12 m-0 fill-white dark:fill-black"
+      class="w-8 h-8 m-0 fill-white dark:fill-black"
     />
     <PauseIcon
       v-else
-      class="w-12 h-12 m-0 fill-white dark:fill-black"
+      class="w-8 h-8 m-0 fill-white dark:fill-black"
     />
   </div>
   <div
     v-else
-    class="flex items-center justify-center h-12 rounded-full aspect-square bg-gray-350 dark:bg-gray-750"
+    class="flex items-center justify-center h-8 rounded-full aspect-square bg-gray-350 dark:bg-gray-750"
   >
-    <PlayIcon class="w-12 h-12 m-0 fill-gray-220 dark:fill-gray-880" />
+    <PlayIcon class="w-8 h-8 m-0 fill-gray-220 dark:fill-gray-880" />
   </div>
 </template>
