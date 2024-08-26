bd94ca692e3454c7e865e8637e6ad5fbf6b62271
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-25 21:03:34 -0400

    Converted shuffle button component to v2

---
+++ src/renderer/src/components/ShuffleButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ShuffleIcon from "@icons/shuffle.svg";
+import { storeToRefs } from "pinia";
+import { useAudio } from "@stores/audio";
+
+const { shuffle } = storeToRefs(useAudio());
+const { toggleShuffle } = useAudio();
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center h-8 aspect-square"
+    @click="toggleShuffle"
+  >
+    <ShuffleIcon
+      v-if="shuffle"
+      class="m-0 transition-all duration-300 ease-in-out fill-green-700 hover:fill-green-800 dark:fill-green-600 hover:dark:fill-green-700"
+    />
+    <ShuffleIcon
+      v-else
+      class="m-0 transition-all duration-300 ease-in-out non-scale-stroke fill-gray-750 hover:fill-gray-600 dark:fill-gray-300 hover:dark:fill-gray-150"
+    />
+  </div>
+</template>
