815ea5b809039b4e78d5f7946272d857de637d88
powerlego <11838540+powerlego@users.noreply.github.com>
2024-08-25 21:08:35 -0400

    Converted skip back button component to v2

---
+++ src/renderer/src/components/SkipBackButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SkipBackIcon from "@icons/previous.svg";
+import { storeToRefs } from "pinia";
+import { useAudio } from "@stores/audio";
+
+const { isAudioAvailable } = storeToRefs(useAudio());
+const { skipPrevious } = useAudio();
+</script>
+
+<template>
+  <div
+    v-if="isAudioAvailable"
+    class="flex items-center justify-center h-8 aspect-square"
+    @click="skipPrevious"
+  >
+    <SkipBackIcon
+      class="m-0 transition-all duration-300 ease-in-out non-scale-stroke fill-gray-750 hover:fill-gray-600 dark:fill-gray-300 hover:dark:fill-gray-150"
+    />
+  </div>
+  <div
+    v-else
+    class="flex items-center justify-center h-8 aspect-square"
+  >
+    <SkipBackIcon class="m-0 fill-gray-350 dark:fill-gray-750" />
+  </div>
+</template>
