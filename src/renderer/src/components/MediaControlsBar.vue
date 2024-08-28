03259b80a082a1570f75f45d392d8cc468868f0a
Nicholas Curl <nicholas_curl@na.honda.com>
2024-08-28 12:24:23 -0400

    Begun to convert the media controls bar component to v2

---
+++ src/renderer/src/components/MediaControlsBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+import CurrentTime from "@components/CurrentTime.vue";
+import Duration from "@components/Duration.vue";
+import PlayButton from "@components/PlayButton.vue";
+import ProgressBar from "@components/ProgressBar.vue";
+import RepeatButton from "@components/RepeatButton.vue";
+import ShuffleButton from "@components/ShuffleButton.vue";
+import SkipBackButton from "@components/SkipBackButton.vue";
+import SkipForwardButton from "@components/SkipForwardButton.vue";
+import SongArtists from "@components/SongArtists.vue";
+import SongDetails from "@components/SongDetails.vue";
+import SongName from "@components/SongName.vue";
+import { storeToRefs } from "pinia";
+import { useAudio } from "@stores/audio";
+import { useMediaKeyBindings } from "@stores/mediaKeyBindings";
+
+const props = withDefaults(defineProps<{
+  progressUpdateInterval?: number;
+  volumeJumpStep?: number;
+}>(), {
+  progressUpdateInterval: 0,
+  volumeJumpStep: 0.05,
+});
+
+const audioStore = useAudio();
+
+const { audio } = storeToRefs(audioStore);
+const { getNextSong, play, pause, skipPrevious, togglePlay, jumpVolume, incrementRepeatMode, toggleMute, toggleShuffle } = audioStore;
+const { keyBindings, combineKeyCodes } = useMediaKeyBindings();
+
+
+const songName = ref("");
+const songArtists = ref("");
+const coverArt = ref("");
+
+</script>
