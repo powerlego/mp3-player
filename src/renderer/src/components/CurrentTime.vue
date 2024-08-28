641bd9ff78fffc80e74c89febe6d786133bafd83
Nicholas Curl <nicholas_curl@na.honda.com>
2024-08-28 06:32:58 -0400

    Converted current time component to v2

---
+++ src/renderer/src/components/CurrentTime.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useAudio } from "@stores/audio";
+
+const audioStore = useAudio();
+const { currentTime, audio } = storeToRefs(audioStore);
+const { updateCurrentTimeDisplay } = audioStore;
+
+onMounted(() => {
+  if (audio.value) {
+    audio.value.addEventListener("timeupdate", updateCurrentTimeDisplay);
+    audio.value.addEventListener("loadedmetadata", updateCurrentTimeDisplay);
+  }
+});
+
+onUnmounted(() => {
+  if (audio.value) {
+    audio.value.removeEventListener("timeupdate", updateCurrentTimeDisplay);
+    audio.value.removeEventListener("loadedmetadata", updateCurrentTimeDisplay);
+  }
+});
+
+</script>
+
+<template>
+  <div class="min-w-[2.5rem] text-xs text-gray-800 dark:text-gray-250 text-right">
+    {{ currentTime }}
+  </div>
+</template>
