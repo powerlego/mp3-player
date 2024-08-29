d4e11d65777bd52f99312082cf6990783e3e52f7
Nicholas Curl <nicholas_curl@na.honda.com>
2024-08-29 08:06:32 -0400

    Formatting

--- src/renderer/src/components/VolumeBar.vue
+++ src/renderer/src/components/VolumeBar.vue
@@ -105,32 +105,54 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div v-if="volume === -1" ref="volumeBar" class="flex flex-row items-center justify-center flex-1 w-full h-5"
-    role="progressbar">
+  <div
+    v-if="volume === -1"
+    ref="volumeBar"
+    class="flex flex-row items-center justify-center flex-1 w-full h-5"
+    role="progressbar"
+  >
     <div class="box-border relative w-full h-1 rounded-full bg-gray-350 dark:bg-gray-750">
       <div class="box-border absolute z-10 h-full bg-gray-300 rounded-full dark:bg-gray-600" />
     </div>
   </div>
-  <div v-else-if="!isAudioAvailable" ref="volumeBar" class="flex flex-row items-center justify-center flex-1 w-full h-5"
-    role="progressbar">
+  <div
+    v-else-if="!isAudioAvailable"
+    ref="volumeBar"
+    class="flex flex-row items-center justify-center flex-1 w-full h-5"
+    role="progressbar"
+  >
     <div class="box-border relative w-full h-1 rounded-full bg-gray-350 dark:bg-gray-750">
       <div class="box-border absolute z-10 h-full bg-gray-300 rounded-full dark:bg-gray-600" />
     </div>
   </div>
-  <div v-else ref="volumeBar" class="flex flex-row items-center justify-center flex-1 w-full h-5 group"
-    role="progressbar" aria-valuemax="100" aria-valuemin="0"
-    :aria-valuenow="Number(currentVolumePosition.split('%')[0])" @mousedown="handleMouseDownOrTouchStart"
-    @touchstart="handleMouseDownOrTouchStart" @contextmenu="($event) => $event.preventDefault()">
+  <div
+    v-else
+    ref="volumeBar"
+    class="flex flex-row items-center justify-center flex-1 w-full h-5 group"
+    role="progressbar"
+    aria-valuemax="100"
+    aria-valuemin="0"
+    :aria-valuenow="Number(currentVolumePosition.split('%')[0])"
+    @mousedown="handleMouseDownOrTouchStart"
+    @touchstart="handleMouseDownOrTouchStart"
+    @contextmenu="($event) => $event.preventDefault()"
+  >
     <div className="relative box-border h-1 w-full rounded-full bg-gray-350 dark:bg-gray-750">
-      <div class="box-border absolute z-20 rounded-full h-13/4 aspect-square group-hover:scale-100"
-        :class="{ 'scale-100': isDragging, 'scale-0': !isDragging }" :style="{ 'left': currentVolumePosition }">
+      <div
+        class="box-border absolute z-20 rounded-full h-13/4 aspect-square group-hover:scale-100"
+        :class="{ 'scale-100': isDragging, 'scale-0': !isDragging }"
+        :style="{ 'left': currentVolumePosition }"
+      >
         <div
           class="absolute h-full w-full top-[calc((50%*-1)+1px)] ml-[calc(50%*-1)] box-border rounded-full shadow shadow-gray-800 dark:shadow-gray-250 group-hover:scale-100 group-hover:bg-gray-550 dark:group-hover:bg-gray-250"
-          :class="{ 'bg-gray-550 dark:bg-gray-250': isDragging, 'bg-gray-450 dark:bg-gray-600': !isDragging }" />
+          :class="{ 'bg-gray-550 dark:bg-gray-250': isDragging, 'bg-gray-450 dark:bg-gray-600': !isDragging }"
+        />
         />
       </div>
-      <div class="box-border absolute z-10 h-full rounded-full group-hover:bg-green-700 dark:group-hover:bg-green-600"
-        :class="{ 'bg-green-700 dark:bg-green-600': isDragging, 'bg-gray-600 dark:bg-gray-250': !isDragging }" />
+      <div
+        class="box-border absolute z-10 h-full rounded-full group-hover:bg-green-700 dark:group-hover:bg-green-600"
+        :class="{ 'bg-green-700 dark:bg-green-600': isDragging, 'bg-gray-600 dark:bg-gray-250': !isDragging }"
+      />
     </div>
   </div>
 </template>
