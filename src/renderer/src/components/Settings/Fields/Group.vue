acc23e840729e6423c2b10e5a5a89a5562f03187
Nicholas Curl <nicholas_curl@na.honda.com>
2024-08-30 13:51:19 -0400

    Begun to convert settings group component to v2

---
+++ src/renderer/src/components/Settings/Fields/Group.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import {
+  Group,
+  SettingsAcceleratorField,
+  SettingsCheckboxField,
+  SettingsColorField,
+  SettingsDirectoryField,
+  SettingsDropdownField,
+  SettingsFileField,
+  SettingsListField,
+  SettingsNumberField,
+  SettingsRadioField,
+  SettingsSliderField,
+  SettingsTextField,
+} from "@/types";
+import AcceleratorField from "./AcceleratorField.vue";
+import CheckboxField from "./CheckboxField.vue";
+
+const fieldMap = {
+  accelerator: AcceleratorField,
+  checkbox: CheckboxField,
+  color: ColorField,
+  directory: DirectoryField,
+  dropdown: DropdownField,
+  file: FileField,
+  group: Group,
+  list: ListField,
+  number: NumberField,
+  radio: RadioField,
+  slider: SliderField,
+  text: TextField,
+};
+}
+
+const props = defineProps<{
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  preferences: { [key: string]: any };
+  group: Group;
+  value?: string;
+}>();
+
+const emit = defineEmits<{
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fieldChange: [key: string, value: any];
+}>();
+
+const fields = computed(() => {
+  return props.group.fields.map((value, idx) => {
+
+  });
+});
+
+</script>
+
+<template>
+  <div :id="'group-' + props.group.id">
+    <div v-if="props.group.label" class="mb-3 text-xl font-bold text-black dark:text-white">
+      {{ props.group.label }}
+    </div>
+  </div>
+</template>
