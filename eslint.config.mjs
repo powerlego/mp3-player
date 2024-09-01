94f6cad69d110ad2c09f7ce3ceac6f7e92847a27
powerlego <11838540+powerlego@users.noreply.github.com>
2024-09-01 08:14:13 -0400

    Updated eslint config to ignore ignored error catches

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -121,6 +121,7 @@ export default tseslint.config(
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
           ignoreRestSiblings: true,
         },
       ],
