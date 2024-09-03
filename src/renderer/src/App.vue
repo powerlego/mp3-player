d2cf5dfc9e9ba559e7dbeb728a9a9dbc2cee1658
powerlego <11838540+powerlego@users.noreply.github.com>
2024-09-03 11:55:10 -0400

    Updated components to use new useTemplateRef macros

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, ref, useTemplateRef } from "vue";
 import { FilePayload } from "@/types";
 import log from "electron-log/renderer";
 import MainWindow from "@components/MainWindow.vue";
@@ -11,7 +11,7 @@ import { useAudio } from "@stores/audio";
 const audioStore = useAudio();
 const { audio } = storeToRefs(audioStore);
 const { setAudio } = audioStore;
-const audioElement = ref<HTMLAudioElement | null>(null);
+const audioElement = useTemplateRef<HTMLAudioElement>("audio");
 const audioSrc = ref("");
 
 function playAudio() {
@@ -47,9 +47,15 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <audio ref="audioElement" :src="audioSrc" preload="none" :controls="false" />
+  <audio
+    ref="audioElement"
+    :src="audioSrc"
+    preload="none"
+    :controls="false"
+  />
   <div
-    class="relative grid w-full h-full min-h-full overflow-y-hidden grid-areas-layout grid-rows-layout grid-cols-layout">
+    class="relative grid w-full h-full min-h-full overflow-y-hidden grid-areas-layout grid-rows-layout grid-cols-layout"
+  >
     <Topbar class="z-10 flex flex-row items-center w-full h-16 m-0 grid-in-main-view justify-evenly" />
     <Sidebar class="z-20 w-56 m-0 text-gray-800 grid-in-nav-bar bg-gray-150 dark:bg-gray-900 dark:text-white" />
     <MainWindow class="flex flex-col w-full overflow-hidden grid-in-main-view bg-gray-180 dark:bg-gray-860" />
