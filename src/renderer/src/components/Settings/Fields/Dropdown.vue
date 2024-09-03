3eefe59935f256beab5bfc288c6eaf507a5a871c
powerlego <11838540+powerlego@users.noreply.github.com>
2024-09-03 11:39:58 -0400

    Fixed import error

--- src/renderer/src/components/Settings/Fields/Dropdown.vue
+++ src/renderer/src/components/Settings/Fields/Dropdown.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 import { SettingsDropdownField } from "@/types";
-import Tooltip from "@components/Tooltip";
+import Tooltip from "@components/Tooltip.vue";
 
 const props = defineProps<{
   field: SettingsDropdownField;
