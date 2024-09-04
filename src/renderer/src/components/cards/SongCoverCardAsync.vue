e1a197ae781c6aca3bebfcca4f5f259cd1da8eea
Nicholas Curl <nicholas_curl@na.honda.com>
2024-09-04 10:36:30 -0400

    Added default values when metadata is missing

--- src/renderer/src/components/cards/SongCoverCardAsync.vue
+++ src/renderer/src/components/cards/SongCoverCardAsync.vue
@@ -23,20 +23,29 @@ const resource = await window.api.getAudioInfo(props.fileLocation).catch((e) => 
 });
 
 const artist = computed(() => {
-  const artists = resource.artist.split(",");
-  if (artists.length > 1) {
-    return artists.join(", ");
+  console.log(resource);
+  if (!resource.artist) {
+    return "Unknown";
   }
-  return artists[0];
+  if (resource.artist.includes(",")) {
+    const artists = resource.artist.split(",");
+    if (artists.length > 1) {
+      return artists.join(", ");
+    }
+    return artists[0];
+  }
+  else {
+    return resource.artist;
+  }
 });
 
 </script>
 <template>
   <ImageCard
-    :line1="resource.title"
+    :line1="resource.title ?? 'Unknown'"
     :line2="artist"
     loading="lazy"
-    :src="`data:image/${resource.pictureFormat};base64,${resource.pictureBase64}`"
+    :src="`data:image/${resource.pictureFormat || 'png'};base64,${resource.pictureBase64 || 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABKklEQVQ4jZWTsUoDQRCFv7'}`"
     :on-click-button="onClickButton"
   >
     <template #icon>
